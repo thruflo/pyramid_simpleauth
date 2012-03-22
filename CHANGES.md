1504fa9a699ead22e2a5f4680aeb6a9bad9fc238
James Arthur <thruflo@gmail.com>
2012-03-22 18:17:28 +0000

    release: add changelog and bump to version `0.2`.

---
+++ CHANGES.md
@@ -0,0 +1,8 @@
+
+# 0.2
+
+Sorted out routes and configurable redirection.
+
+# 0.1
+
+Initial version.
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def _read(file_name):
 
 setup(
     name = 'pyramid_simpleauth',
-    version = '0.1',
+    version = '0.2',
     description = 'Session based authentication and role based security \
                    for a Pyramid web application.',
     author = 'James Arthur',
